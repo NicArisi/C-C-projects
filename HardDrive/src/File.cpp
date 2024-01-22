050200f117c05a554123340762342e6fdd994679
Nicolas F. Arisi Fernandez <nicoarisi339@gmail.com>
2024-01-21 23:18:02 -0500

    more stuff

--- HardDrive/src/File.cpp
+++ HardDrive/src/File.cpp
@@ -1,16 +1,20 @@
 #include "File.h"
 std::vector<File*> Files;
-File::File(std::string data, std::string Name){
-  this->data=data;
-  this->Name=Name;
+File::File(std::string data, std::string Name) {
+	this->data = data;
+	this->Name = Name;
 }
-File::File(std::string Name){this->Name=Name;}
-void File::addFiles(File* file){
-  Files.push_back(file);
+File::File(std::string Name) {
+	this->Name = Name;
 }
-void File::changeData(std::string data){
-  this->data=data;
+void File::addFiles(File *file) {
+	Files.push_back(file);
 }
-File::~File(){
-
+void File::changeData(std::string data) {
+	this->data = data;
+}
+File::~File() {
+	for (int i = 0; i < Files.size(); i++) {
+		delete (Files[i]);
+	}
 }
--- HardDrive/src/Main.cpp
+++ HardDrive/src/Main.cpp
@@ -2,29 +2,32 @@
 #include "File.h"
 #include "HardDrive.h"
 int main() {
-  File* file1 = new File("Junk");
-  File* file2 = new File("Hello World", "HW.txt");
-  File* file3 = new File("h", "h.txt");
-  File* file4 = new File("1020203039", "nums.txt");
-  File* file5 = new File("23424324", "nums.txt");
-  file1->addFiles(file2);
-  file1->addFiles(file3);
-  file1->addFiles(file4);
-  file1->addFiles(file5);
-  /*
-  std::cout << file1->Files.size() << std::endl;
-  std::cout << file1->Files[0]->data << std::endl;
-  std::cout << file1->Files[1]->data << std::endl;
-  std::cout << file1->Files[2]->data << std::endl;
-  */
-  HardDrive* NUm1 = new HardDrive(1, 1000, 4);
-  NUm1->AppendFile(file1);
-  std::cout << NUm1->fileBook.size()<< std::endl;
-  std::vector<File*> fileCollectionMain=NUm1->ReturnFilesOfName("nums.txt");
-  for(int i=0;i<fileCollectionMain.size();i++){
-    std::cout <<"The Data of this name index is " << fileCollectionMain[i]->data<< std::endl;
-  }
-  for(int i=0;i<NUm1->fileBook.size();i++){
-   std::cout << "The Name of " << i << " index is " << NUm1->fileBook[i]->Name<< std::endl;
-  }
+	File *file1 = new File("Junk");
+	File *file2 = new File("Hello World", "HW.txt");
+	File *file3 = new File("h", "h.txt");
+	File *file4 = new File("1020203039", "nums.txt");
+	File *file5 = new File("23424324", "nums.txt");
+	file1->addFiles(file2);
+	file1->addFiles(file3);
+	file1->addFiles(file4);
+	file1->addFiles(file5);
+	/*
+	 std::cout << file1->Files.size() << std::endl;
+	 std::cout << file1->Files[0]->data << std::endl;
+	 std::cout << file1->Files[1]->data << std::endl;
+	 std::cout << file1->Files[2]->data << std::endl;
+	 */
+	HardDrive *NUm1 = new HardDrive(1, 1000, 4);
+	NUm1->AppendFile(file1);
+	std::cout << NUm1->fileBook.size() << std::endl;
+	std::vector<File*> fileCollectionMain = NUm1->ReturnFilesOfName("nums.txt");
+	for (int i = 0; i < fileCollectionMain.size(); i++) {
+		std::cout << "The Data of this name index is "
+				<< fileCollectionMain[i]->data << std::endl;
+	}
+	for (int i = 0; i < NUm1->fileBook.size(); i++) {
+		std::cout << "The Name of " << i << " index is "
+				<< NUm1->fileBook[i]->Name << std::endl;
+	}
+	delete (NUm1);
 }
